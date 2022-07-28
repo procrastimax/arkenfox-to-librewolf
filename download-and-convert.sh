68ba44efe664bee9c98b82d086fd58c81811a6b3
procrastimax <mmeheykeroth@protonmail.com>
2022-07-28 22:05:59 +0200

    Now creates backups before copying new .cfg file

--- download-and-convert.sh
+++ download-and-convert.sh
@@ -27,14 +27,29 @@ DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS="$HOME/.librewolf"
 echo "Converting $INPUT_FILENAME to $OUTPUT_FILENAME"
 
 if [ -d "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK" ]; then
+    if [ -f "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK"/"$OUTPUT_FILENAME" ]; then
+        echo "Creating backup of old .cfg file"
+        cp -v "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK"/"$OUTPUT_FILENAME" "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK"/"$OUTPUT_FILENAME".bak
+    fi
+
     echo "Saving $OUTPUT_FILENAME in $DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK"
     sed "s/user_pref/defaultPref/g" "$INPUT_FILENAME" >> "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK"/"$OUTPUT_FILENAME"
 
 elif [ -d "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS" ]; then
+    if [ -f "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS"/"$OUTPUT_FILENAME" ]; then
+        echo "Creating backup of old .cfg file"
+        cp -v "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS"/"$OUTPUT_FILENAME" "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS"/"$OUTPUT_FILENAME".bak
+    fi
+
     echo "Saving $OUTPUT_FILENAME in $DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS"
     sed "s/user_pref/defaultPref/g" "$INPUT_FILENAME" >> "$DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS"/"$OUTPUT_FILENAME"
 
 else
+    if [ -f "$ABS_SCRIPT_PATH"/"$OUTPUT_FILENAME" ]; then
+        echo "Creating backup of old .cfg file"
+        cp -v "$ABS_SCRIPT_PATH"/"$OUTPUT_FILENAME" "$ABS_SCRIPT_PATH"/"$OUTPUT_FILENAME".bak
+    fi
+
     echo "Neither $DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_FLATPAK nor $DEFAULT_LIBREWOLF_OVERRIDE_CFG_PATH_MACOS exist, saving $OUTPUT_FILENAME in $ABS_SCRIPT_PATH"
     echo "Please make sure to find the location, in which the $OUTPUT_FILENAME should be put in on yourself! (here is a guide: https://librewolf.net/docs/settings/)"
     sed "s/user_pref/defaultPref/g" "$INPUT_FILENAME" >> "$ABS_SCRIPT_PATH"/"$OUTPUT_FILENAME"
